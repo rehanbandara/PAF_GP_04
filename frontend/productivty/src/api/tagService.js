29510929eb8321dca82bd8c390bd5b4697fdeef0
BANDARA R <72589921+rehanbandara@users.noreply.github.com>
2026-04-25 17:19:05 +0530

    Delete frontend directory

--- frontend/productivty/src/api/tagService.js
+++
@@ -1,9 +0,0 @@
-import api from './axiosInstance';
-
-export const tagService = {
-  getAll: () => api.get('/api/tags'),
-  getById: (id) => api.get(`/api/tags/${id}`),
-  create: (data) => api.post('/api/tags', data),
-  update: (id, data) => api.put(`/api/tags/${id}`, data),
-  delete: (id) => api.delete(`/api/tags/${id}`),
-};
